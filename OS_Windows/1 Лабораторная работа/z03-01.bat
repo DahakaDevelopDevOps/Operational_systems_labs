64c714cee7dd4ebca87e25f6ff73be6cb21612b8
DahakaDevelop <typebooleanfalse@gmail.com>
2023-09-01 16:20:38 +0300

    add first lab

---
+++ OS_Windows/1 Лабораторная работа/z03-01.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Username: %USERNAME%
+echo User Domain: %USERDOMAIN%
+echo Name of PC: %COMPUTERNAME%
+echo Current Date: %DATE%
+echo Current time: %TIME%
+pause
---
+++ OS_Windows/1 Лабораторная работа/z03-02.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Name Of file: %~nx0
+echo Date of Creation: %~t0
+echo Path to file: %~dp0
+pause
