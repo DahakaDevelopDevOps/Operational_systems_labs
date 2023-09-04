4a1ac80823873580498f93843c15172cd2d522b8
DahakaDevelop <typebooleanfalse@gmail.com>
2023-09-04 12:02:02 +0300

    add screensots for both laboratory works

--- OS_Windows/1 Лабораторная работа/z03-01.bat
+++ OS_Windows/1 Лабораторная работа/z03-01.bat
@@ -1,7 +1,7 @@
 @echo off
-echo Username: %USERNAME%
-echo User Domain: %USERDOMAIN%
-echo Name of PC: %COMPUTERNAME%
-echo Current Date: %DATE%
-echo Current time: %TIME%
+echo Имя: %USERNAME%
+echo Пользовательский домен: %USERDOMAIN%
+echo Имя компьютера: %COMPUTERNAME%
+echo Текущая дата: %DATE%
+echo Текущее время: %TIME%
 pause
